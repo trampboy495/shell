b4f5eda39b8b08715bb3e774bf32291c8e41bb6c
trampboy <histroylover@gmail.com>
2020-03-21 16:06:39 +0530

    employee check attendance program created

--- empWagecomputation.sh
+++ empWagecomputation.sh
@@ -2,4 +2,11 @@
 
 
 echo "Welcome to Employmee wage calculation program"
-
+isPresent=1
+randomcheck=$((RANDOM % 2))
+if [ $isPresent -eq $randomcheck ]
+then
+echo "Employee is present"
+else
+echo "Employee is not present"
+fi 
