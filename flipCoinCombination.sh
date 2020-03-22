3859e12ad81cb5eae4f1e0561697cdf426b45057
trampboy495 <histroylover@gmail.com>
2020-03-22 16:45:48 +0530

    flip coin doublet combination stored and percentage determine

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -21,6 +21,29 @@ coinSinglet[$i]="T"
 fi
 ((i++))
 done
-Headpercentage=$(($noOfheads*10))
-
+HeadpercentageSinglet=$(($noOfheads*10))
+noOfheads=0
+noOftails=0
+i=0
+while [ $i -lt 10 ]
+do
+r=$((RANDOM%2))
+r2=$((RANDOM%2))
+if [ $r -eq 1 -a  $r2 -eq 1 ]
+then
+coinDoublet[$i]="HH"
+noOfheads=$(($noOfheads+1))
+elif [ $r -eq 0 -a  $r2 -eq 0 ]
+then
+coinDoublet[$i]="TT"
+((noOftails++))
+elif [ $r -eq 0 -a  $r2 -eq 1 ]
+then
+coinDoublet[$i]="TH"
+else
+coinDoublet[$i]="HT"
+fi
+((i++))
+done
+HeadpercentageDoublet=$(($noOfheads*10))
 
