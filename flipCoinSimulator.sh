fcb3eb94f36c655a52c0b02ca3c0c8383c216989
trampboy495 <histroylover@gmail.com>
2020-03-22 12:37:46 +0530

    simulator looped multiple times and displayed no of tails and no of heads

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -x
 
-
+i=0
+noOfheads=0
+noOftails=0
+while [ $i -lt 4 ]
+do
 r=$((RANDOM%2))
 if [ $r -eq 1 ]
 then
-echo "Heads"
+((noOfheads++))
 else
-echo "Tails"
+((noOftails++))
 fi
+((i++))
+done
+echo "no of heads $noOfheads"
+echo "no of tails $noOftails"
