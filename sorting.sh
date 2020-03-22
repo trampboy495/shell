394ff143688ab1de0e22c16b58333348392be5ff
trampboy495 <histroylover@gmail.com>
2020-03-22 11:42:06 +0530

    computation values read from dictionary to array

--- sorting.sh
+++ sorting.sh
@@ -12,4 +12,9 @@ com3=$((c+a/b))
 com4=$((a%b+c))
 declare -A computation
 computation=([1]=$com1 [2]=$com2 [3]=$com3 [4]=$com4)
+counter=0
+for i in ${!computation[@]}
+do
+array[ ((counter++)) ]=${computation[$i]}
+done
 
