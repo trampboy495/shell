3296f369a092cf32b43ec1d799243c68deb13e06
trampboy495 <histroylover@gmail.com>
2020-03-31 18:02:57 +0530

    email and lastname validation added

--- userRegisteration.sh
+++ userRegisteration.sh
@@ -5,3 +5,14 @@
 read -p "enter user frist name: " fristname
 regex1='^[A-Z][a-z]{2,}$'
 [[ $fristname =~ $regex1 ]] && { echo "valid";} || { echo "invalid"; }
+
+read -p "enter user last name: " lastname
+regex1='^[A-Z][a-z]{2,}$'
+[[ $lastname =~ $regex1 ]] && { echo "valid";} || { echo "invalid"; }
+
+read -p "enter user email: " email
+regex1='^(\<abc\>)+([\+\-\.][0-9a-zA-Z]+)*(@bl\.co)+([\.][a-z]{2})*$'
+[[ $email =~ $regex1 ]] && { echo "valid";} || { echo "invalid"; }
+
+
+
